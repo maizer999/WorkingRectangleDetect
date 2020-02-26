1b752598e0d9ea421818d71c53dcf527186c31c7
bob <maizer99@gmail.com>
2020-02-26 22:06:31 +0200

    Minor updates

--- app/src/main/java/com/example/nativeopencvandroidtemplate/MainActivity.java
+++ app/src/main/java/com/example/nativeopencvandroidtemplate/MainActivity.java
@@ -52,11 +52,11 @@ public class MainActivity extends AppCompatActivity {
         OpenCVLoader.initDebug();
     }
 
-    public void applyFilter(View v){
+    public void applyFilter(View v) {
         Mat img = null;
 
         try {
-            img = Utils.loadResource(getApplicationContext(), R.drawable.test6);
+            img = Utils.loadResource(getApplicationContext(), R.drawable.test1);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -65,101 +65,106 @@ public class MainActivity extends AppCompatActivity {
 
         Mat img_result = img.clone();
 
-        findSquares(img_result ,squares);
-
+        findSquares(img_result, squares);
 
 
         Imgproc.polylines(img_result, squares, true, new Scalar(255, 255, 0), 10);
 
-        for(int i =0 ; i< squares.size() ; i++) {
 
-        for( Point p: squares.get(i).toList() ){
+        Rect roi = new Rect(10, 150, 200, 100);
+        Mat cropped = new Mat(img_result, roi);
 
-Log.d("X = "+p.x," y = "+p.y);
-        }}
+
+        for (int i = 0; i < squares.size(); i++) {
+
+            for (Point p : squares.get(i).toList()) {
+//            Rect rectCrop = new Rect(squares.get(i).toList().get(0).x ,squares.get(i).toList().get(1).x , squares.get(i).toList().get(3).x, squares.get(i).toList().get(4).x);
+
+
+                Log.d("X = " + p.x, " y = " + p.y);
+            }
+        }
 
 //        Imgproc.Canny(img, img_result, 80, 90);
 //        Bitmap img_bitmap = Bitmap.createBitmap(img_result.cols(), img_result.rows(),Bitmap.Config.ARGB_8888);
 //        Utils.matToBitmap(img_result, img_bitmap);
+
+//        img_result
         final Bitmap bitmap =
                 Bitmap.createBitmap(img_result.cols(), img_result.rows(), Bitmap.Config.ARGB_8888);
         Utils.matToBitmap(img_result, bitmap, true);
+
+//        final Bitmap bitmap =
+//                Bitmap.createBitmap(cropped.cols(), cropped.rows(), Bitmap.Config.ARGB_8888);
+//        Utils.matToBitmap(cropped, bitmap, true);
+
 
         ImageView imageView = findViewById(R.id.img);
         imageView.setImageBitmap(bitmap);
     }
 
 
-
-
     List<MatOfPoint> squares = new ArrayList<MatOfPoint>();
 
 
-
-
-    int thresh = 50, N = 11;
+    int  N = 11;
 
     // helper function:
     // finds a cosine of angle between vectors
     // from pt0->pt1 and from pt0->pt2
-    double angle( Point pt1, Point pt2, Point pt0 ) {
+    double angle(Point pt1, Point pt2, Point pt0) {
         double dx1 = pt1.x - pt0.x;
         double dy1 = pt1.y - pt0.y;
         double dx2 = pt2.x - pt0.x;
         double dy2 = pt2.y - pt0.y;
-        return (dx1*dx2 + dy1*dy2)/Math.sqrt((dx1*dx1 + dy1*dy1)*(dx2*dx2 + dy2*dy2) + 1e-10);
+        return (dx1 * dx2 + dy1 * dy2) / Math.sqrt((dx1 * dx1 + dy1 * dy1) * (dx2 * dx2 + dy2 * dy2) + 1e-10);
     }
 
 
     // returns sequence of squares detected on the image.
     // the sequence is stored in the specified memory storage
-    void findSquares( Mat image, List<MatOfPoint> squares )
-    {
+    void findSquares(Mat image, List<MatOfPoint> squares) {
 
         squares.clear();
 
-        Mat smallerImg=new Mat(new Size(image.width()/2, image.height()/2),image.type());
+        Mat smallerImg = new Mat(new Size(image.width() / 2, image.height() / 2), image.type());
 
-        Mat gray=new Mat(image.size(),image.type());
+        Mat gray = new Mat(image.size(), image.type());
 
-        Mat gray0=new Mat(image.size(),CvType.CV_8U);
+        Mat gray0 = new Mat(image.size(), CvType.CV_8U);
 
         // down-scale and upscale the image to filter out the noise
         Imgproc.pyrDown(image, smallerImg, smallerImg.size());
         Imgproc.pyrUp(smallerImg, image, image.size());
-        Double x1=0.0 ;
-        Double x2 = 0.0;
+
 
 
         // find squares in every color plane of the image
-        for( int c = 0; c < 3; c++ )
-        {
+        for (int c = 0; c < 3; c++) {
 
             extractChannel(image, gray, c);
 
             // try several threshold levels
-            for( int l = 1; l < N; l++ )
-            {
+            for (int l = 1; l < N; l++) {
                 //Cany removed... Didn't work so well
 
 
-                Imgproc.threshold(gray, gray0, (l+1)*255/N, 255, Imgproc.THRESH_BINARY);
+                Imgproc.threshold(gray, gray0, (l + 1) * 255 / N, 255, Imgproc.THRESH_BINARY);
 
 
-                List<MatOfPoint> contours=new ArrayList<MatOfPoint>();
+                List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
 
                 // find contours and store them all as a list
                 Imgproc.findContours(gray0, contours, new Mat(), Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
-
-                MatOfPoint approx=new MatOfPoint();
+                MatOfPoint approx = new MatOfPoint();
 
                 // test each contour
-                for( int i = 0; i < contours.size(); i++ )
-                {
+                for (int i = 0; i < contours.size(); i++) {
 
                     // approximate contour with accuracy proportional
                     // to the contour perimeter
-                    approx = approxPolyDP(contours.get(i),  Imgproc.arcLength(new MatOfPoint2f(contours.get(i).toArray()), true)*0.02, true);
+                    approx = approxPolyDP(contours.get(i), Imgproc.arcLength(new MatOfPoint2f(contours.get(i).toArray()), true) * 0.02, true);
+
 
 
                     // square contours should have 4 vertices after approximation
@@ -169,25 +174,23 @@ Log.d("X = "+p.x," y = "+p.y);
                     // area may be positive or negative - in accordance with the
                     // contour orientation
 
-                    if( approx.toArray().length == 4 &&
-                            Math.abs(Imgproc.contourArea(approx)) > 1000 &&   Math.abs(Imgproc.contourArea(approx)) < 100000 &&
-                            Imgproc.isContourConvex(approx) )
-                    {
+                    if (approx.toArray().length == 4 &&
+                            Math.abs(Imgproc.contourArea(approx)) > 1000 && Math.abs(Imgproc.contourArea(approx)) < 100000 &&
+                            Imgproc.isContourConvex(approx)) {
 
 
                         double maxCosine = 0;
 
-                        for( int j = 2; j < 5; j++ )
-                        {
+                        for (int j = 2; j < 5; j++) {
                             // find the maximum cosine of the angle between joint edges
-                            double cosine = Math.abs(angle(approx.toArray()[j%4], approx.toArray()[j-2], approx.toArray()[j-1]));
+                            double cosine = Math.abs(angle(approx.toArray()[j % 4], approx.toArray()[j - 2], approx.toArray()[j - 1]));
                             maxCosine = Math.max(maxCosine, cosine);
                         }
 
                         // if cosines of all angles are small
                         // (all angles are ~90 degree) then write quandrange
                         // vertices to resultant sequence
-                        if( maxCosine < 0.3 )
+                        if (maxCosine < 0.3)
                             squares.add(approx);
                     }
                 }
@@ -196,27 +199,25 @@ Log.d("X = "+p.x," y = "+p.y);
     }
 
     void extractChannel(Mat source, Mat out, int channelNum) {
-        List<Mat> sourceChannels=new ArrayList<Mat>();
-        List<Mat> outChannel=new ArrayList<Mat>();
+        List<Mat> sourceChannels = new ArrayList<Mat>();
+        List<Mat> outChannel = new ArrayList<Mat>();
 
         Core.split(source, sourceChannels);
 
-        outChannel.add(new Mat(sourceChannels.get(0).size(),sourceChannels.get(0).type()));
+        outChannel.add(new Mat(sourceChannels.get(0).size(), sourceChannels.get(0).type()));
 
-        Core.mixChannels(sourceChannels, outChannel, new MatOfInt(channelNum,0));
+        Core.mixChannels(sourceChannels, outChannel, new MatOfInt(channelNum, 0));
 
         Core.merge(outChannel, out);
     }
 
     MatOfPoint approxPolyDP(MatOfPoint curve, double epsilon, boolean closed) {
-        MatOfPoint2f tempMat=new MatOfPoint2f();
+        MatOfPoint2f tempMat = new MatOfPoint2f();
 
         Imgproc.approxPolyDP(new MatOfPoint2f(curve.toArray()), tempMat, epsilon, closed);
 
         return new MatOfPoint(tempMat.toArray());
     }
-
-
 
 
 }
